29d40fa5a2821c0c579f29dbbd63d62276fbcc10
mike.rs <mike@mike.rs>
2018-03-31 20:34:57 -0400

    ignore api credentials (revoked old ones)

--- src/setup/install.sh
+++ src/setup/install.sh
@@ -1,6 +1,6 @@
 
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ zesty-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
 	  sudo apt-key add -
@@ -17,3 +17,8 @@ pwd=$PWD
 sudo -u postgres $pwd/setup_postgres.sh $user
 
 ) > db_password
+
+
+
+sudo apt-get install python-pip
+sudo pip install -r requirements.txt
