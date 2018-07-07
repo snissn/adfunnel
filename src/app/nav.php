3797b8e943c8bc2efb2b74a4119dc1cdd2a14c0c
mike.rs <mike@mike.rs>
2018-07-06 21:09:32 -0400

    update nav bar

--- src/app/nav.php
+++ src/app/nav.php
@@ -1,5 +1,7 @@
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
     <link href="/custom.css" rel="stylesheet">
+    <link href="/responsive.css" rel="stylesheet">
     <link href="/responsive.css" rel="stylesheet">
 <style>
 .logo{float: left;
@@ -14,7 +16,35 @@ width: 100%;}
 .navbar-nav ml-auto{float: left;
 width: 100%;}
 
+.navbar .nav>li {
+    padding: 0 15px;
+    border-top: 3px solid transparent;
+    border-radius: 0;
+    margin-right: 1px;
+}
 
+ li.active >a {
+    color: #029445 !important;
+}
+.navbar .nav>li>a {
+    color: #212121;
+    font-size: 15px;
+    line-height: 20px;
+    font-weight: 700;
+    transition: none;
+    text-transform: uppercase;
+    font-family: Poppins;
+		padding: 20px 0;
+}
+.navbar-nav {
+    padding-right: 120px;
+}
+
+@media (min-width: 768px)
+.navbar-right {
+    float: right!important;
+    margin-right: -15px;
+}
 </style>
 
 
@@ -29,8 +59,16 @@ width: 100%;}
         </button>
 		</div>
 		<div class="main_text">
-		<h1>Cryptocurrency Ranking & Sentiment Analysis</h1>
 		
+<div class="main-menu">
+  <ul id="menu-top-menu" class="menu nav navbar-nav navbar-right">
+    <li id="menu-item-321" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-304 current_page_item menu-item-321 has-menu-child"><a title="Home" href="http://www.adfunnel.tech/">Home</a></li>
+    <li id="menu-item-318" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-318  has-menu-child"><a title="About" href="http://www.adfunnel.tech/about/">About</a></li>
+    <li id="menu-item-322" class="menu-item menu-item-type-custom active  menu-item-object-custom menu-item-322  has-menu-child"><a title="Cryptocurrency Sentiment" href="http://beta.adfunnel.tech">Cryptocurrency Sentiment</a></li>
+    <li id="menu-item-320" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-320  has-menu-child"><a title="News" href="http://www.adfunnel.tech/news/">News</a></li>
+    <li id="menu-item-319" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-319  has-menu-child"><a title="Submit Press Release" href="http://www.adfunnel.tech/submit-press-release/">Submit Press Release</a></li>
+  </ul>                    
+</div>
 		</div>
 
   </div>
